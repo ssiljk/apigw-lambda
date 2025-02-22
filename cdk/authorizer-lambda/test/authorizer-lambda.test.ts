5f74d1b99d11a61dc7de7413523dcc64e27754ae
leonjesusC <leonjesus@outlook.com>
2025-02-22 19:16:37 -0300

    add cdk projects and dotnet projects

---
+++ cdk/authorizer-lambda/test/authorizer-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AuthorizerLambda from '../lib/authorizer-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/authorizer-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AuthorizerLambda.AuthorizerLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
